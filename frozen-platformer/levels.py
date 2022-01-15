0b6926303ac161869a70dfddcd871960362fcd97
Manuel Saelices <msaelices@gmail.com>
2022-01-15 23:31:05 +0100

    Add player and enemies into the level. No controls and behavior yet

--- frozen-platformer/levels.py
+++ frozen-platformer/levels.py
@@ -1,19 +1,19 @@
 import pygame
 
 from config import TILE_SIZE
-from sprites import Tile
+from sprites import Enemy, Player, Tile
 
 levels_layout = [
     [
         '                    ',
         '   XXXX         XXXX',
-        '                    ',
+        '          E         ',
         '         XXX        ',
         '                    ',
-        'X                   ',
-        'X                   ',
+        'X       E           ',
+        'XP                  ',
         'XX                 X',
-        'XXXX     XX       XX',
+        'XXXX     XX      EXX',
         'XXXX             XXX',
     ],
     [
@@ -39,15 +39,27 @@ class Level:
         self.setup(level_layout)
 
     def setup(self, layout: list[str]):
+        self.enemies = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
         self.tiles = pygame.sprite.Group()
         for y, row in enumerate(layout):
             for x, cell in enumerate(row):
+                cell_pos = (x * TILE_SIZE, y * TILE_SIZE)
                 if cell == 'X':
-                    tile = Tile(TILE_SIZE, (x * TILE_SIZE, y * TILE_SIZE))
+                    tile = Tile(TILE_SIZE, cell_pos)
                     self.tiles.add(tile)
+                elif cell == 'P':
+                    player = Player(cell_pos)
+                    self.player.add(player)
+                elif cell == 'E':
+                    enemy = Enemy(cell_pos)
+                    self.enemies.add(enemy)
+
 
     def draw(self):
         self.tiles.draw(self.surface)
+        self.player.draw(self.surface)
+        self.enemies.draw(self.surface)
 
     def move_x(self, x):
         self.tiles.update(x)
--- frozen-platformer/sprites.py
+++ frozen-platformer/sprites.py
@@ -1,6 +1,7 @@
 from pygame import Surface
 from pygame.sprite import Sprite
 
+from config import GRAVITY, TERMINAL_VELOCITY
 
 class Tile(Sprite):
 
@@ -12,3 +13,23 @@ class Tile(Sprite):
 
     def update(self, x_shift: int):
         self.rect.x += x_shift
+
+
+class Entity(Sprite):
+
+    def __init__(self, pos: tuple):
+        super().__init__()
+        # TODO: show real sprites
+        self.image = Surface((30, 60))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect(topleft=pos)
+        self.vel_x = 0
+        self.vel_y = 0
+
+
+class Enemy(Entity):
+    color = 'red'
+
+
+class Player(Entity):
+    color = 'green'
